aa96fc50b9d3191ef1795fac53fabad2e2fdaec3
Valentyn-Dominskyi <valentyn2202@gmail.com>
2021-09-21 11:13:50 +0300

    ✨: update creating car and routes

--- docs/Tables/MySQL/CREATE_ROUTES.sql
+++ docs/Tables/MySQL/CREATE_ROUTES.sql
@@ -2,16 +2,16 @@ USE MEDIVAC;
 
 DROP TABLE ROUTES;
 
-CREATE TABLE `ROUTES` (
+CREATE TABLE `routes` (
   `ID` int unsigned NOT NULL AUTO_INCREMENT,
   `NAME` varchar(45) NOT NULL,
   `PLACE_FROM` varchar(45) NOT NULL,
   `PLACE_TO` varchar(45) NOT NULL,
   `PRICE` double NOT NULL,
   `TIME` datetime NOT NULL,
-  `CAR` int NOT NULL,
+  `CAR` int unsigned NOT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `ID_UNIQUE` (`ID`),
-  KEY `CAR_idx` (`CAR`),
-  CONSTRAINT `CAR` FOREIGN KEY (`CAR`) REFERENCES `car` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  KEY `CAR_idx` (`CAR`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+
