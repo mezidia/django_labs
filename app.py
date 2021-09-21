3772c738134fc2988dc15370bf72a1f658410491
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-09-21 09:43:05 +0300

    Create simple program: set title and geometry
    Tkinter is built-in library, so just import and start to code

---
+++ app.py
@@ -0,0 +1,11 @@
+from tkinter import *
+import tkinter.messagebox as MessageBox
+
+# import mysql
+
+root = Tk()
+root.geometry('600x300')
+root.title('Autostation')
+root.resizable(width=False, height=False)
+
+root.mainloop()
