f1cfbc5f7adbfb5a57849f86f7b305aa8d7c21d2
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-09-21 09:46:30 +0300

    Add labels: add three labels with text
    In future I want to make it trough loop

--- app.py
+++ app.py
@@ -8,4 +8,13 @@ root.geometry('600x300')
 root.title('Autostation')
 root.resizable(width=False, height=False)
 
+id = Label(root, text='Enter id', font=('bold', 10))
+id.place(x=20, y=30)
+
+name = Label(root, text='Enter name', font=('bold', 10))
+name.place(x=20, y=60)
+
+phone = Label(root, text='Enter phone', font=('bold', 10))
+phone.place(x=20, y=90)
+
 root.mainloop()
