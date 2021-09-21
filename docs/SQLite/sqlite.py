ff9b4f61a0376248859764ba9feaaa08f5a702f6
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-09-21 13:19:31 +0300

    Add new method: write create_table function
    User can create table by typing the table name and list of fields

--- docs/SQLite/sqlite.py
+++ docs/SQLite/sqlite.py
@@ -1,6 +1,8 @@
 import sqlite3
 from sqlite3 import Error
 
+from typing import List
+
 
 class SQLite:
     def __init__(self, path: str):
@@ -21,3 +23,35 @@ class SQLite:
             raise f"The error '{e}' occurred"
         self.connection = connection
 
+    def create_table(self, table_name: str, fields: List[str]):
+        """
+        Method for creating the table
+
+        Example:
+            SQLite().create_table('users', [
+                'id INTEGER PRIMARY KEY AUTOINCREMENT',
+                'name TEXT NOT NULL',
+                'age INTEGER',
+                'gender TEXT',
+                'nationality TEXT'
+            ])
+
+        :param table_name: name of the table, that you want to create
+        :param fields: list of the fields as strings
+        :return:
+        """
+        cursor = self.connection.cursor()
+        fields_string = ''
+        for index in range(len(fields)):
+            if index == len(fields) - 1:
+                fields_string += fields[index]
+            else:
+                fields_string += fields[index] + ',\n'
+        print(fields_string)
+        query = f'CREATE TABLE IF NOT EXISTS {table_name} ({fields_string});'
+        try:
+            cursor.execute(query)
+            self.connection.commit()
+            print("Query executed successfully")
+        except Error as e:
+            raise f"The error '{e}' occurred"
