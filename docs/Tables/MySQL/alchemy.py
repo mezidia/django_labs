6ca618aa5aa42e51c0da3a77a7751ffe21b5c729
Valentyn-Dominskyi <valentyn2202@gmail.com>
2021-09-23 22:56:44 +0300

    ✨: adding new rows is working

--- docs/Tables/MySQL/alchemy.py
+++ docs/Tables/MySQL/alchemy.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Integer, String, Column, DateTime, \
- ForeignKeyConstraint, Float, create_engine
+from sqlalchemy import Integer, String, Column, DateTime, Float,\
+PrimaryKeyConstraint, ForeignKeyConstraint, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session
 from config import *
@@ -9,23 +9,28 @@ Base = declarative_base()
 
 class Route(Base):
     __tablename__ = 'routes'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer)
     name = Column(String(45), nullable=False)
     place_from = Column(String(45), nullable=False)
     place_to = Column(String(45), nullable=False)
     price = Column(Float, nullable=False)
     time = Column(DateTime(), nullable=False)
-    car = Column(Integer)
+    car = Column(Integer, nullable=False)
 
     __table_args__ = (
+        PrimaryKeyConstraint('id', name='id_pk'),
         ForeignKeyConstraint(['car'], ['car.id']),
     )
 
 
 class Car(Base):
     __tablename__ = 'car'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer)
     name = Column(String(45), nullable=False)
+
+    __table_args__ = (
+        PrimaryKeyConstraint('id', name='id_pk'),
+    )
 
 
 engine = create_engine(f'mysql://{user}:{password}@{localhost}/{db_name}')
@@ -33,12 +38,14 @@ Base.metadata.create_all(engine)
 session = Session(bind=engine)
 
 car_Medivac = Car(
-    id = 4,
     name = 'medivac'
 )
 
+session.add(car_Medivac)
+
+session.commit()
+
 route_Dragon_Shrine = Route(
-    id = 5,
     name = 'FIRST SILVER EXPRESS',
     place_from = 'ALTERAC PASS',
     place_to = 'DRAGON SHRINE',
@@ -47,8 +54,6 @@ route_Dragon_Shrine = Route(
     car = car_Medivac.id
 )
 
-session.add(car_Medivac)
-session.commit()
-
 session.add(route_Dragon_Shrine)
+
 session.commit()
