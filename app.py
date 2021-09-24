34f8bbd770f1dcf9080c10f56026d3587555c406
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-09-24 22:29:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,67 +5,68 @@ import tkinter.messagebox as message_box
 # import mysql
 
 def insert():
-    id = e_id.get()
-    name = e_name.get()
-    phone = e_phone.get()
+    id = e_route_name.get()
+    name = e_place_from.get()
+    phone = e_place_to.get()
 
     if id == '' or name == '' or phone == '':
         message_box.showerror('Insert Status', 'All fields are required')
     else:
         # mysql
-        e_id.delete(0, 'end')
-        e_name.delete(0, 'end')
-        e_phone.delete(0, 'end')
+        e_route_name.delete(0, 'end')
+        e_place_from.delete(0, 'end')
+        e_place_to.delete(0, 'end')
         show()
         message_box.showinfo('Insert Status', 'Inserted successfully')
 
 
 def delete():
-    if e_id.get() == '':
+    if e_route_name.get() == '':
         message_box.showerror('Delete Status', 'ID is compulsory for delete')
     else:
         # mysql
-        e_id.delete(0, 'end')
-        e_name.delete(0, 'end')
-        e_phone.delete(0, 'end')
+        e_route_name.delete(0, 'end')
+        e_place_from.delete(0, 'end')
+        e_place_to.delete(0, 'end')
         show()
         message_box.showinfo('Delete Status', 'Deleted successfully')
 
 
 def update():
-    id = e_id.get()
-    name = e_name.get()
-    phone = e_phone.get()
+    id = e_route_name.get()
+    name = e_place_from.get()
+    phone = e_place_to.get()
 
     if id == '' or name == '' or phone == '':
         message_box.showerror('Update Status', 'All fields are required')
     else:
         # mysql
-        e_id.delete(0, 'end')
-        e_name.delete(0, 'end')
-        e_phone.delete(0, 'end')
+        e_route_name.delete(0, 'end')
+        e_place_from.delete(0, 'end')
+        e_place_to.delete(0, 'end')
         show()
         message_box.showinfo('Update Status', 'Updated successfully')
 
 
 def get():
-    if e_id.get() == '':
+    if e_route_name.get() == '':
         message_box.showerror('Fetch Status', 'ID is compulsory for fetch')
     else:
         # mysql
-        message_box.showinfo('Fetch Status', e_id)
+        message_box.showinfo('Fetch Status', e_route_name)
 
 
 def show():
-    list.delete(0, list.size())
     rows = [
         [1, 'dasdasdad'],
         [2, 'dadaddad']
     ]
-    list.delete(0, list.size())
+    routes_list.delete(0, routes_list.size())
+    cars_list.delete(0, cars_list.size())
     for row in rows:
-        insertData = str(row[0]) + ' ' * 10 + str(row[1])
-        list.insert(list.size() + 1, insertData)
+        insert_data = str(row[0]) + ' ' * 10 + str(row[1])
+        routes_list.insert(routes_list.size() + 1, insert_data)
+        cars_list.insert(cars_list.size() + 1, insert_data)
 
 
 root = Tk()
@@ -73,38 +74,65 @@ root.geometry('600x300')
 root.title('Autostation')
 root.resizable(width=False, height=False)
 
-id = Label(root, text='Enter id', font=('bold', 10))
-id.place(x=20, y=30)
+route_name = Label(root, text='Name of the route', font=('bold', 10))
+route_name.place(x=20, y=30)
 
-name = Label(root, text='Enter name', font=('bold', 10))
-name.place(x=20, y=60)
+place_from = Label(root, text='Enter place from', font=('bold', 10))
+place_from.place(x=20, y=60)
 
-phone = Label(root, text='Enter phone', font=('bold', 10))
-phone.place(x=20, y=90)
+place_to = Label(root, text='Enter place to', font=('bold', 10))
+place_to.place(x=20, y=90)
 
-e_id = Entry()
-e_id.place(x=150, y=30)
+price = Label(root, text='Enter price', font=('bold', 10))
+price.place(x=20, y=120)
 
-e_name = Entry()
-e_name.place(x=150, y=60)
+time = Label(root, text='Enter time', font=('bold', 10))
+time.place(x=20, y=150)
 
-e_phone = Entry()
-e_phone.place(x=150, y=90)
+car = Label(root, text='Enter car id', font=('bold', 10))
+car.place(x=20, y=180)
+
+routes_label = Label(root, text='Available routes', font=('bold', 10))
+routes_label.place(x=290, y=30)
+
+cars_label = Label(root, text='Available cars', font=('bold', 10))
+cars_label.place(x=430, y=30)
+
+e_route_name = Entry()
+e_route_name.place(x=150, y=30)
+
+e_place_from = Entry()
+e_place_from.place(x=150, y=60)
+
+e_place_to = Entry()
+e_place_to.place(x=150, y=90)
+
+e_price = Entry()
+e_price.place(x=150, y=120)
+
+e_time = Entry()
+e_time.place(x=150, y=150)
+
+e_car = Entry()
+e_car.place(x=150, y=180)
 
 insert = Button(root, text='insert', font=('italic', 10), bg='white', command=insert)
-insert.place(x=20, y=140)
+insert.place(x=20, y=260)
 
 delete = Button(root, text='delete', font=('italic', 10), bg='white', command=delete)
-delete.place(x=70, y=140)
+delete.place(x=70, y=260)
 
 update = Button(root, text='update', font=('italic', 10), bg='white', command=update)
-update.place(x=130, y=140)
+update.place(x=130, y=260)
 
 get = Button(root, text='get', font=('italic', 10), bg='white', command=get)
-get.place(x=190, y=140)
+get.place(x=190, y=260)
 
-list = Listbox(root)
-list.place(x=290, y=30)
+routes_list = Listbox(root)
+routes_list.place(x=290, y=50)
+
+cars_list = Listbox(root)
+cars_list.place(x=430, y=50)
 show()
 
 root.mainloop()
