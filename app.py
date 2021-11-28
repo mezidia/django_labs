11b07282b4df614d96cf762c508e49f8cd7af0c9
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-11-28 20:55:51 +0200

    Update app.py
    Set the model for flights

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+class Flight(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    plane = db.Column(db.String(200), nullable=False)
+    destination_from = db.Column()
+    destination_to = db.Column()
+    flight_created = db.Column(db.DateTime, default=datetime.utcnow())
+    departure_date = db.Column(db.DateTime)
+    arrival_date = db.Column(db.DateTime)
+    duration = db.Column(db.Float)
+    price = db.Column(db.Float)
+
+    def __repr__(self):
+        return f'<Flight {self.id}>'
 
 
 @app.route('/')
