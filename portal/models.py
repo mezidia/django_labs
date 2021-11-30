2c413e963229a704a3834a93610993cf85878858
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-11-30 11:21:16 +0200

    Create model
    Co-Authored-By: Valentyn-Dominskyi <valentyn2202@gmail.com>

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Flight(models.Model):
+    id = models.IntegerField(primary_key=True)
+    plane = models.CharField(max_length=200, null=False)
+    destination_from = models.CharField(max_length=200, default='Лос Сантос')
+    destination_to = models.CharField(max_length=200, default='Сан Фіерро')
+    flight_created = models.DateTimeField(auto_now_add=True)
+    departure_date = models.CharField(max_length=200)
+    arrival_date = models.CharField(max_length=200)
+    duration = models.CharField(max_length=200)
+    price = models.FloatField()
--- portal/views.py
+++ portal/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'portal/index.html')
+
+
+def flights(request):
+    return render(request, 'portal/index.html')
